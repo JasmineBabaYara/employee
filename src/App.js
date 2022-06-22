d049d85b9b192aaf86282e1e26601499e6c6555c
JasmineBabaYara <jassyprincess1@icloud.com>
2022-06-22 16:09:12 -0700

    screen sizes added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+
+//importing respective components
 import Nav from "./components/Nav";
 import Dashboard from "./components/Navscreens/Dashboard";
 import Project from "./components/Navscreens/Project";
 import Events from "./components/Navscreens/Events";
 import Activities from "./components/Navscreens/Activities";
 
-
 function App() {
   return (
-    <div >
+    <>
       <Nav />
       <Routes>
-      <Route path='/activities' element={<Activities />} />
-      <Route path='/events' element={<Events />} />
-      <Route path='/project' element={<Project />} />
-        <Route path='/' element={<Dashboard />} />
+        <Route path="/activities" element={<Activities />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/" element={<Dashboard />} />
       </Routes>
-    </div>
+    </>
   );
 }
 
